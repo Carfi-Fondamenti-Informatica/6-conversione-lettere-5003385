46ebccfeeeeafbb7e788a3d75672243296c34a56
5003385 <91500260+5003385@users.noreply.github.com>
2021-11-30 17:14:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+int main() {
 
-int main(){
-  return 0;
+    char a;
+    cin >> a;
+
+    if (lib(a)== true){
+        cout << a << endl;
+    } else {
+        cout << "errore" << endl;
+    }
+    return 0;
 }

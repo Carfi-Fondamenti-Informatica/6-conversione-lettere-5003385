d7916b5f2ff6423f49c2326db3028998811f370c
5003385 <91500260+5003385@users.noreply.github.com>
2021-11-30 17:13:54 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,25 @@
 
+
+#include "lib.h"
+#include <cctype>
+
+bool lib(char &a){
+
+    if (isalpha(a)){
+
+        int c=a;
+
+
+        if (c<96){
+            c= c+32;
+            a=c;
+        } else {
+            c=c-32;
+            a=c;
+        }
+        return true;
+
+    } else if (isdigit(a)){
+        return false;
+    }
+}
